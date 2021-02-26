6fa7e591c2904b81d275df5710f49a93e867108c
EmrehliugGui <79599382+EmrehliugGui@users.noreply.github.com>
2021-02-26 15:26:03 -0300

    Add files via upload

---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 EMREHLIUG GM 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIC PAY,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/+5564999544094*
+║
+╚═〘 GM BOT 〙
+`
+}
+exports.iklan = iklan
